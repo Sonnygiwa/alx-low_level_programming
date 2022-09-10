9ceefc1ef7f0fef1fe8f8b5d0407d857157d2d6b
Sonnygiwa <mr.seungiwa@gmail.com>
2022-09-10 15:18:38 -0700

    Task 6: Size C program that prints the size of various types on the computer

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Printing the size of various types
+ * Return: 0
+ */
+int main (void)
+{
+	char m;
+	int n;
+	long int o;
+	long long int p;
+	float q;
+
+	printf("Size of a char: %d byte(s)\n", sizeof(m));
+	printf("Size of int: %d byte(s)\n", sizeof(n));
+	printf("Size of long long int: %d byte(s)\n", sizeof(p));
+	printf("Size of long int: %d byte(s)\n", sizeof(o));
+	printf("Size of float: %d byte(s)\n", sizeof(q));
+	return (0);
+}
