a25e870e46c8c619d88e94177962f520996a2f30
Sonnygiwa <mr.seungiwa@gmail.com>
2022-09-11 14:21:41 -0700

    Task 4 Program that print the alphabet in lowercase with a new line

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: C program that  print all aplhabet letters except q and e
+ * followed by a new line
+ * Return: 0
+*/
+
+int main(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		if (alphabet == 'e' || alphabet == 'q')
+			alphabet++;
+		putchar(alphabet);
+		alphabet++;
+	}
+	putchar('\n');
+
+	return (0);
+}
