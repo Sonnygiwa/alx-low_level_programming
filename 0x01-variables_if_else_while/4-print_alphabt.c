2a18c4d63e3d30bda5743d376b4aaf07be8f8267
Sonnygiwa <mr.seungiwa@gmail.com>
2022-09-11 14:35:58 -0700

    Task 4 Program that print the alphabet in lowercase with a new line

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -13,7 +13,8 @@ int main(void)
 
 	while (alphabet <= 'z')
 	{
-		if (alphabet == 'e' || alphabet == 'q') alphabet++;
+		if (alphabet == 'e' || alphabet == 'q') 
+			alphabet++;
 		putchar(alphabet);
 		alphabet++;
 	}
