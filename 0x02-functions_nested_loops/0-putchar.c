7a276ed66c69a652a6b5163a7afc624188bf7558
Sonnygiwa <mr.seungiwa@gmail.com>
2022-09-13 14:35:40 -0700

    TAsk 0 _putchar Program that print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - main function
+ * Description: C program that prints _putchar
+ * followed by a new
+ * Return: 0
+ */
+int main(void)
+{
+	char c[] = "_putchar";
+	int times = 0;
+	while (times < 9)
+	{
+		_putchar(c[times]);
+		times++;
+	}
+	_putchar('\n');
+	return (0);
+}
