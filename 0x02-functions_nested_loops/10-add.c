9a002dbcd6e645c5c75a64fc062016a4e265af2a
Sonnygiwa <mr.seungiwa@gmail.com>
2022-09-14 14:39:07 -0700

    Task 10 10-add.c adding two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include"main.h"
+
+/**
+ * add - adding two integers and 
+ * returns the result
+ * @a: first input
+ * @b: second input
+ * Return: result (sum a+b)
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
