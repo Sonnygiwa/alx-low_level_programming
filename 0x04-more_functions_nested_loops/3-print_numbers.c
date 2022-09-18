99b30157bf56b724206e0bd6d3df1a7df2ba77b1
Sonnygiwa <mr.seungiwa@gmail.com>
2022-09-18 13:45:46 -0700

    Task 3 3-print_numbers.c Printing the num from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print_numbers - print numbers from 0-9
+ *Return: always 0
+ */
+
+void print_numbers(void)
+{
+	void print_numbers(void)
+{
+       int num = '0';
+       while (num <= '9')
+       {
+	       _putchar (num);
+	       ++num;
+       }
+       _putchar ('\n');
+       return;
+}
