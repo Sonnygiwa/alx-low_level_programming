550459d63776c28d86116d6bebc51398807ecbeb
Sonnygiwa <mr.seungiwa@gmail.com>
2022-09-20 14:53:56 -0700

    HTask 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers between 0 to 14
+ * Ten Times
+ */
+
+void more_numbers(void)
+{
+	int c;
+	int count;
+
+	count = 0;
+
+	for (count = 0; count < 10; count++)
+	{
+		for (c = 0; c < 15; c++)
+		{
+			if (c >= 10)
+				putchar((c / 10) + 48);
+			putchar((c % 10) + 48);
+		}
+		putchar('\n');
+	}
+}
