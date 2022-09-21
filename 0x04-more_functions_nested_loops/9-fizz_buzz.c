7fae2a373abcb7f987099877c5b0fe93c592977b
Sonnygiwa <mr.seungiwa@gmail.com>
2022-09-21 13:58:09 -0700

    Task 9 Fizz-Buzz printing 1 to 100

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,19 +15,19 @@ int main(void)
 	{
 		if (z % 3 == 0 && z % 15 != 0)
 		{
-			printf(" Fizz");
+			printf("Fizz");
 		}
 		else if (z % 5 == 0 && z % 15 != 0)
 		{
-			printf(" Buzz");
+			printf("Buzz");
 		}
 		else if (z % 15 == 0)
 		{
-			printf(" FizzBuzz");
+			printf("FizzBuzz");
 		}
 		else
 		{
-			printf(" %d", z);
+			printf("%d", z);
 		}
 	}
 	printf("\n");
