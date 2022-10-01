5c25e823ce134181d520f3eac482fa35bcc0981e
Sonnygiwa <mr.seungiwa@gmail.com>
2022-10-01 15:23:50 -0700

    Task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ *_strcmp - compares strings
+ *@s1: string character
+ *@s2: string character
+ *Return: a pointer to string
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int a = 0;
+
+	while (s1[a] == s2[a] && s1[a] && s2[a])
+	{
+		a++;
+	}
+
+	return (s1[a] - s2[a]);
+}
