ccd92b422ae9c144cd45484aae9152d923c3b8ee
Sonnygiwa <mr.seungiwa@gmail.com>
2022-10-08 14:34:39 -0700

    TAsk 10

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * base10 - power in 10 base
+ * @n: an exponent
+ * Return: returns 10 to power exponent
+ */
+
+void print_number(int n)
+{
+	unsigned int n1;
+	
+	n1 = n;
+	
+	if (n < 0)
+	{
+		putchar('-');
+		n1 = -n;
+	}
+	
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+	putchar((n1 % 10) + '0');
+}
